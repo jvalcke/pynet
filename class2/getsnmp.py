a8ba68295a1f4f85f44ea8fa779e17a6521460d0
Jeroen Valcke <jeroen@valcke.com>
2016-02-16 06:03:48 -0800

    scipt to get snmp info

---
+++ class2/getsnmp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from snmp_helper import snmp_get_oid, snmp_extract
+
+def get_snmp_data(device, OID):
+    return snmp_extract(snmp_get_oid(device,OID))
+
+def main():
+    IP_ADDR='50.76.53.27'
+    SNMP_PORT=7961
+    COMMUNITY_STRING='galileo'
+    SYS_NAME_OID='1.3.6.1.2.1.1.5.0'
+    SYS_DESCR_OID='1.3.6.1.2.1.1.1.0'
+
+    pynet_rtr1 = (IP_ADDR, COMMUNITY_STRING, 7961)
+    pynet_rtr2 = (IP_ADDR, COMMUNITY_STRING, 8061)
+
+    devices = [ pynet_rtr1, pynet_rtr2 ]
+    for device in devices:
+        print device
+        output = get_snmp_data(device,SYS_NAME_OID) 
+        print output
+        output = get_snmp_data(device,SYS_DESCR_OID)
+        print output
+
+if __name__ == '__main__':
+    main()
